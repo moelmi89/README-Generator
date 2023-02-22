466ac9b93ede8993f8ae3ae106dc3da23124d285
Moelmi89 <moelmi89@gmail.com>
2023-02-22 22:04:10 +0000

    array of questions added and write2function fix

--- index.js
+++ index.js
@@ -1,10 +1,48 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: "input",
+    name: "Title",
+    message: "What is the title of your project"
+},
+{
+    type: "input",
+    name: "Table of contents",
+    message: "Provide a table of contents"
+},
+{
+    type: "input",
+    name:
+    message:
+},
+{
+    type: "input",
+    name:
+    message:
+},
+{
+    type: "input",
+    name:
+    message:
+}];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (error) => {
+        console.log(fileName)
+        console.log(data)
+        if (error) {
+            return console.log(error)
+        } else {
+            console.log("success");
+        }
+    })
+}
 
 // TODO: Create a function to initialize app
 function init() {}
