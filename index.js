4eeb2c48e766e16acb599c9ebccce6f629d02b98
Moelmi89 <moelmi89@gmail.com>
2023-02-22 23:08:17 +0000

    questions array completed pacakgelock file created

--- index.js
+++ index.js
@@ -6,30 +6,51 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [{
-    type: "input",
-    name: "Title",
-    message: "What is the title of your project"
-},
-{
-    type: "input",
-    name: "Table of contents",
-    message: "Provide a table of contents"
-},
-{
-    type: "input",
-    name:
-    message:
-},
-{
-    type: "input",
-    name:
-    message:
-},
-{
-    type: "input",
-    name:
-    message:
-}];
+        type: "input",
+        name: "Title",
+        message: "What is the title of your project"
+    },
+    {
+        type: "input",
+        name: "Description",
+        message: "Provide a detailed description of your project"
+    },
+    {
+        type: "input",
+        name: "Table of contents (Optional)",
+        message: "Provide a table of contents"
+    },
+    {
+        type: "input",
+        name: "Installation",
+        message: "What are the steps required to install your project? Provide a step-by-step description."
+    },
+    {
+        type: "input",
+        name: "Usage",
+        message: "Provide instructions and examples for use. Include screenshots as needed."
+    },
+    {
+        type: "input",
+        name: "Credits",
+        message: "List your collaborators, if any, with links to their GitHub profiles"
+    },
+    {
+        type: "input",
+        name: "License",
+        message: "Provide a a list of licenses used"
+    },
+    {
+        type: "input",
+        name: "Contributions",
+        message: "Provide guidelines for how other developers can contribute"
+    },
+    {
+        type: "input",
+        name: "Tests",
+        message: "Write tests for the application"
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
